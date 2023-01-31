d4a153082d2a6edc58af08176bb20d4832bbb207
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-01-31 19:12:22 +0700

    status code

--- tutorial/03-clients-and-server/serve.js
+++ tutorial/03-clients-and-server/serve.js
@@ -4,32 +4,35 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   const { url } = req;
 
+  res.setHeader('Content-Type', 'text/html');
+
   let path = './views';
   switch (url) {
     case '/':
-      path += '/index.html'; 
+      path += '/index.html';
+      res.statusCode = 200;
       break;
     case '/about':
       path += '/about.html';
+      res.statusCode = 200;
       break;
     case '/blog':
       path += '/blog.html';
+      res.statusCode = 200;
       break;
       // oh jadi kalau gak di break mah bisa aja jadi /blog.html/404.html
     default:
       path += '/404.html';
+      res.statusCode = 404;
       break;
   }
-
-  console.log(path);
 
   fs.readFile(path, (error, data) => {
     if (error) {
       console.log(error);
       res.statusCode = 500;
     } else {
-      res.setHeader('Content-Type', 'text/html')
-        .end(data);
+      res.end(data);
       /**
        * kalau pakai res.write(data).end() gak tahu kenapa gak jalan
        * tapi kalau pakai res.write(data); res.end() jalan
