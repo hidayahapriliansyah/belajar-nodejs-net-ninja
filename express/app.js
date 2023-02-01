e70945a0e8fa16477de557288bd9ae3222bad78f
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-02-01 17:05:13 +0700

    09 mongodb

--- express/app.js
+++ express/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 const app = express();
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://nodeninja:test12345678@node-ninja.jqk12sf.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 // listening port
-app.listen(3000);
+// app.listen(3000);
 
 // middleware 
 // app.use((req, res, next) => {
@@ -38,22 +47,44 @@ app.set('view engine', 'ejs');
 // app.set('views', './myviews');
 // untuk mengganti folder views bisa menggunakan app.set('views', 'directory');
 
+// app.get('/add-blog', (req, res) => {
+//   const blog = new Blog({
+//     title: 'new blog',
+//     snippet: 'blog snippet',
+//     body: 'full body of blog',
+//   });
+
+//   blog.save()
+//     .then((result) => {
+//       res.send(result)
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// });
+
 app.get('/', (req, res) => {
   // res.sendFile('./views/index.html', { root: __dirname });
   // res.render akan merender file ejs.
   // path akan langsung mengarah ke folder views
   // folder views merupakan folder defaul tempat menyimpan file ejs
-  const blogs = [
-    { title: 'Blog1', snippet: 'Lorem ipsum dolor sit amet' },
-    { title: 'Blog2', snippet: 'Lorem ipsum dolor sit amet' },
-    { title: 'Blog3', snippet: 'Lorem ipsum dolor sit amet' },
-  ];
-  res.render('index', { title: 'Home', blogs });
+
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   // res.sendFile('./views/about.html', { root: __dirname });
   res.render('about', { title: 'About' });
+});
+
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', { title: 'All Blogs', blogs: result })
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 app.get('/blogs/create', (req, res) => {
